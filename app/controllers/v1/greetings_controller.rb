591c1255861c4d154cf36f196740ad3ed2a9a145
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-22 18:15:07 +0200

    Fix issue

--- app/controllers/v1/greetings_controller.rb
+++
@@ -1,8 +0,0 @@
-class V1::GreetingsController < ApplicationController
-  def index
-    @messages = Greeting.find(Greeting.pluck(:id).sample)
-    render json: {
-      message: @messages.message
-    }
-  end
-end
