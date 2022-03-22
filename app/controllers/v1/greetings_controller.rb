50a5f22c764d86dad5a8e6d5b8bb84f3dee0c27d
Daniel Shdeed <87448628+Danieldotcomcoder@users.noreply.github.com>
2022-03-22 17:14:28 +0200

    Fix controller

--- app/controllers/v1/greetings_controller.rb
+++ app/controllers/v1/greetings_controller.rb
@@ -1,8 +1,8 @@
 class V1::GreetingsController < ApplicationController
   def index
-    greeting = Greeting.find(Greeting.pluck(:id).sample)
+    @messages = Greeting.find(Greeting.pluck(:id).sample)
     render json: {
-      message: greeting.content
+      message: @messages.message
     }.to_json
   end
 end
